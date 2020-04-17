69540aa1da78bfe1f69420b7d704d0b988116a96
Tales Dias <miletotg@Gmail.com>
2020-04-17 03:43:21 -0300

    Repositories Add and Remove

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const[repositories,setRepositories] =useState([]);
+ 
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data);      
+    })
+  },[]);
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories',{
+      title: 'Repositorio Tales',
+      url: 'https://github.com/talesdg/conceitos-reactjs',
+      techs: [ 'ReactJS']
+    });
+    const repository = response.data;
+    setRepositories([...repositories,repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`);
+    setRepositories([]);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      {repositories.map(repository =>  
+        <li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(`${repository.id}`)}>
             Remover
           </button>
         </li>
+      )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
